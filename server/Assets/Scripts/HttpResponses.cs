eb837c6eabc41d87fcd278509b11bb9ecdd5d1d0
Logan Dhillon <logandhi64@gmail.com>
2023-11-16 22:42:18 -0500

    Create connection request class

--- server/Assets/Scripts/HttpResponses.cs
+++ server/Assets/Scripts/HttpResponses.cs
@@ -2,14 +2,24 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public class HttpResponseData {
-	public string message;
-
-	public HttpResponseData(string message) {
-		this.message = message;
-	}
-
+public class HttpData {
 	public string ToJson() {
 		return JsonUtility.ToJson(this);
+	}
+}
+
+public class HttpResponse_Generic: HttpData {
+	public string message;
+
+	public HttpResponse_Generic(string message) {
+		this.message = message;
+	}
+}
+
+public class HttpRequest_ConnectPlayer: HttpData {
+	public string name;
+
+	public HttpRequest_ConnectPlayer(string name) {
+		this.name = name;
 	}
 }
